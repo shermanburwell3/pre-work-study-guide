7058777ad8121deafdd810df820f9e2452a35e75
Sherman Burwell <shermanburwell3@gmail.com>
2024-04-27 17:48:29 -0500

    refined javascript code

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,17 +1,26 @@
 const topics = ["HTML","CSS","Git","JavaScript"];
-
-/* if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-} */
-
-for (let x = 0; x<topics.length; x++) {
-    console.log("Let's study " + topics[x] + "!")
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+const topic = randomTopic;
+function listTopics() {
+  for (let x = 0; x<topics.length; x++) {
+      console.log(topics[x]);
+  }
 }
+
+function selectTopic() {
+   if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  } 
+}
+
+
+listTopics();
+selectTopic();
